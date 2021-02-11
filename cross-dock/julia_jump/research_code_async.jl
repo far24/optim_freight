1aaa0101e7c59937558010a5a3683d2e405fae3d
far24 <fahim.radi@gmail.com>
2021-02-11 00:44:57 -0500

    asynch model code written; needs further testing and update

--- cross-dock/julia_jump/research_code_async.jl
+++ cross-dock/julia_jump/research_code_async.jl
@@ -3,10 +3,10 @@
 #*****************************************
 using Random
 using JuMP
+using GLPK, Cbc
 using LinearAlgebra
-using GLPK
-
 using CSV
+using Distances
 
 # vehicle routing problem with cross-dock
 # Wen et al. (2009)
@@ -15,19 +15,23 @@ using CSV
 # Network Property
 #*****************************************
 # suppliers are the pick-up nodes
-P = [1,2,3]
+P = [1,2]
 
 # customers are the delivery nodes
-D = [4,5,6]
+D = [3,4]
 
 # cross-docks
-cross_pickup = [7]
-cross_delivery = [8]
+cd_pick_start = [D[end]+1]
+cd_pick_end = [D[end]+2]
+cd_del_start = [D[end]+3]
+cd_del_end = [D[end]+4]
 
-N = vcat(P,D, cross_pickup, cross_delivery)
+
+
+N = vcat(P,D, cd_pick_start, cd_pick_end, cd_del_start, cd_del_end)
 
 # vehicles
-K = [1, 2]
+K = [1]
 
 # ****************************************
 # parameters
@@ -35,15 +39,16 @@ K = [1, 2]
 # number of pickup/delivery Nodes
 n = length(P)
 
-d_i = [10,20,30, 30, 20, 10]
+d_i = [10,20,10,20, 1,1,1,1]
 veh_cap = 40
 
 # location of nodes on a graph
-c = c = [9,5,4,8,13,11,8,8]
+#c = [9,5,4,8,13,11,8,8]
+c = [90,50,150,130,80,80,80,80]
 t_ij = pairwise(Euclidean(), c'; dims=2)
+t_ij
 
-
-
+#=
 # [ai , bi] = the time window for node i (i ∈ N);
 a_i = zeros(N)
 b_i = rand(5:10, N)
@@ -58,6 +63,8 @@ Q = 50
 A = rand(5:10, O)
 # B = the time for unloading and reloading a pallet.
 A = rand(5:10, O)
+=#
+
 
 # very big number
 M= 99999
@@ -81,19 +88,19 @@ cd_modl = Model(GLPK.Optimizer)
 @variable(cd_modl, x_ijk[i=N,j=N,k=K], Bin)
 @variable(cd_modl, u_ik[i=P, k=K], Bin)
 @variable(cd_modl, r_ik[i=P, k=K], Bin)
-@variable(cd_modl, q_k[k=K], Bin)
-@variable(cd_modl, qq_k[k=K], Bin)
+@variable(cd_modl, g_k[k=K], Bin)
+@variable(cd_modl, h_k[k=K], Bin)
 
 # CONTINUOUS
 # ^^^^^^^^^^^^
 # l_ik = the time at which vehicle k leaves node i (i ∈ N, k ∈ K); 0 otherwise;
 # f_k = finish time of unloading operation of truck k at cross-dock (k ∈ K)
 # g_i = finish time of unloading operation of request i at cross-dock (i ∈ P)
-# s_k = start time of reloading operation of truck k at cross-dock (k ∈ K)
-@variable(cd_modl, l_ik[i=N, k=K] >= 0)
-@variable(cd_modl, f_k[k=K] >= 0)
-@variable(cd_modl, s_k[k=K] >= 0)
-@variable(cd_modl, g_i[i=P] >= 0)
+# w_k = start time of reloading operation of truck k at cross-dock (k ∈ K)
+@variable(cd_modl, s_ik[i=N, k=K] >=0 )
+@variable(cd_modl, f_k[k=K]>= 0)
+@variable(cd_modl, w_k[k=K] >= 0)
+@variable(cd_modl, v_i[i=P] >= 0)
 
 
 # ****************************************
@@ -102,63 +109,67 @@ cd_modl = Model(GLPK.Optimizer)
 
 # --------------------[Pick-up Process]
 # 01: only one vehicle can arrive at a pickup node
-@constraint(cd_modl, p_nd_entry[i= P],
-                    sum(x_ijk[i,j,k] for j=P for k=K if i != j) == 1)
+@constraint(cd_modl, p_nd_exit[i= P],
+                    sum(x_ijk[i,j,k] for j=vcat(P, cd_pick_end) for k=K if i != j) == 1)
 
 
 # 02: start from cross dock // all the trucks must be utilized
-@constraint(cd_modl, p_nd_start[i= cross_pickup, k=K],
+@constraint(cd_modl, p_nd_start[i= cd_pick_start, k=K],
                     sum(x_ijk[i,j,k] for j=P if i != j) == 1)
 
 # 03: end at cross dock
-@constraint(cd_modl, p_nd_end[j= cross_pickup, k=K],
+@constraint(cd_modl, p_nd_end[j= cd_pick_end, k=K],
                 sum(x_ijk[i,j,k] for i=P if i != j) == 1)
 
 # 04: capacity of vehicle
 @constraint(cd_modl, p_veh_cap[k=K],
-                sum(d_i[i] * x_ijk[i,j,k] for i=P for j=P if i != j) <= veh_cap)
+                sum(d_i[i] * x_ijk[i,j,k] for i=P for j=vcat(P,cd_pick_end) if i != j) <= veh_cap)
 
 # 05: continuous routes/ consecutive movement
 @constraint(cd_modl, p_move[h=P, k=K],
-                    sum(x_ijk[i,h,k] for i=P if i != h)
-                    - sum(x_ijk[h,j,k] for j=P if j != h) == 0)
+                    sum(x_ijk[i,h,k] for i=vcat(cd_pick_start,P) if i != h)
+                    - sum(x_ijk[h,j,k] for j=vcat(P,cd_pick_end) if j != h) == 0)
 
 
 # --------------------[delivery Process]
 # 01: only one vehicle can arrive at a pickup node
-@constraint(cd_modl, d_nd_entry[i=D],
-                    sum(x_ijk[i,j,k] for j=D for k=K if i != j) == 1)
+@constraint(cd_modl, d_nd_exit[i=D],
+                    sum(x_ijk[i,j,k] for j=vcat(D, cd_del_end) for k=K if i != j) == 1)
 
 # 02: start from cross dock // all the trucks must be utilized
-@constraint(cd_modl, d_nd_start[i= cross_delivery, k=K],
+@constraint(cd_modl, d_nd_start[i= cd_del_start, k=K],
                 sum(x_ijk[i,j,k] for j=D if i != j) == 1)
 
 # 03: end at cross dock
-@constraint(cd_modl, d_nd_end[j= cross_delivery, k=K],
+@constraint(cd_modl, d_nd_end[j= cd_del_end, k=K],
                 sum(x_ijk[i,j,k] for i=D if i != j) == 1)
 
 # 04: capacity of vehicle
 @constraint(cd_modl, d_veh_cap[k=K],
-                sum(d_i[i] * x_ijk[i,j,k] for i=D for j=D if i != j) <= veh_cap)
+                sum(d_i[i] * x_ijk[i,j,k] for i=D for j=vcat(D,cd_del_end) if i != j) <= veh_cap)
 
 # 05: continuous routes/ consecutive movement
 @constraint(cd_modl, d_move[h=D, k=K],
-                    sum(x_ijk[i,h,k] for i=D if i != h)
-                    - sum(x_ijk[h,j,k] for j=D if j != h) == 0)
+                    sum(x_ijk[i,h,k] for i=vcat(cd_del_start, D) if i != h)
+                    - sum(x_ijk[h,j,k] for j=vcat(D,cd_del_end) if j != h) == 0)
 
 
 # --------------------[time constraints]
-# 01: arrival time of vehicle at node if
-@constraint(cd_modl, arr_time[i=N,j=N,k=K; i != j],
-                    l_ik[j,k]
-                    - l_ik[i,k]
+# 01: arrival time of vehicle at pickup nodes
+@constraint(cd_modl, arr_time_pick[i=vcat(cd_pick_start, P, cd_pick_end),j=vcat(cd_pick_start, P, cd_pick_end),k=K; i != j],
+                    s_ik[j,k]
+                    - s_ik[i,k]
                     - t_ij[i,j]
                     + M*(1-x_ijk[i,j,k])
                      >= 0)
 
-@constraint(cd_modl, arr_time_init[i=N,j=N,k=K; i != j],
-                    l_ik[j,k] <= M*x_ijk[i,j,k])
-
+# arrival time of vehicle at delivery nodes
+@constraint(cd_modl, arr_time_del[i=vcat(cd_del_start, D, cd_del_end),j=vcat(cd_del_start, D, cd_del_end),k=K; i != j],
+                 s_ik[j,k]
+                 - s_ik[i,k]
+                 - t_ij[i,j]
+                 + M*(1-x_ijk[i,j,k])
+                  >= 0)
 
 # --------------------[consolidation decision]
 
@@ -166,62 +177,82 @@ cd_modl = Model(GLPK.Optimizer)
 @constraint(cd_modl, consolidation[i=P,k=K],
                 u_ik[i,k]
                 - r_ik[i,k]
-                - sum(x_ijk[i,j,k] for j = vcat(P,cross_pickup) if i!=j)
-                + sum(x_ijk[i+n,j,k] for j = vcat(D,cross_delivery)if i+n !=j)
+                - sum(x_ijk[i,j,k] for j = vcat(P,cd_pick_end) if i!=j)
+                + sum(x_ijk[i+n,j,k] for j = vcat(D,cd_del_end)if i+n !=j)
                 == 0)
-# unloading and loading does not happen for same vehicles
+# unloading and loading does not happen for same vehicles at cross dock
 @constraint(cd_modl, load_unload[i=P,k=K],
                 u_ik[i,k]
                 + r_ik[i,k]
                 <= 1)
 
-# check for unloading process of truck at cross-docks
+
+
+# check (1 and 2) if the vehicles has to stop at cross-dock using decision from request
+# this variable is used to calculate the unloading time of vehicle next
 @constraint(cd_modl, unload_check1[k=K],
-                (1/M)* sum(u_ik[i,k] for i=P) - q_k[k] <= 0)
+                (1/M)*sum(u_ik[i,k] for i=P) <= g_k[k])
 
 @constraint(cd_modl, unload_check2[k=K],
-                q_k[k] - sum(u_ik[i,k] for i=P) <= 0)
+                g_k[k] <= sum(u_ik[i,k] for i=P))
+
 
 # Calculate Finish time of trucks for unloading
 @constraint(cd_modl, unld_f_tm_veh[k=K],
                 f_k[k]
-                == l_ik[cross_pickup[1],k]
-                + 2*q_k[k]
-                + 0.05*sum(d_i[i]*u_ik[i,k] for i=P)
+                == s_ik[cd_pick_end[1],k]
+                + 1*g_k[k]
+                + 2*sum(d_i[i]*u_ik[i,k] for i=P)
                 )
 
-# calculate finish time of unloading demand/request "i"
-@constraint(cd_modl, unld_f_tm_req[i=P,k=K],
-            g_i[i] >= f_k[k] - M*(1-u_ik[i,k]))
+# unloading time of request i = finish time of veh k (f_k) if veh k unloads
+# req i (u_ik) at the cross dock
+@constraint(cd_modl, unld_req_tm[i=P, k=K],
+            v_i[i]>= f_k[k] - M*(1-u_ik[i,k])
+            )
 
-# calculate start time of reloading in the trucks
-@constraint(cd_modl, strt_reld_tm_veh[i=P,k=K],
-            s_k[k] >= g_i[i] - M*(1-r_ik[i,k]))
-
-# reloading starts after unloading
+# reloading in trucks (w_k) starts after the finish time (f_k) of unloading at cross docks
+# of the same truck
 @constraint(cd_modl, unld_reld[k=K],
-            s_k[k]>= f_k[k])
+            w_k[k]>= f_k[k])
 
 
-# check 01 for reloading process of truck at cross-docks
+@constraint(cd_modl, reld_start_tm[i=P, k=K],
+            w_k[k]>= v_i[i] - M*(1-r_ik[i,k]))
+
+# check (1 and 2) if the vehicles has to stop at cross-dock using decision from request
+# this variable is used to calculate the unloading time of vehicle next
 @constraint(cd_modl, reload_check1[k=K],
-                (1/M)* sum(r_ik[i,k] for i=P) - qq_k[k] <= 0)
+                (1/M)*sum(r_ik[i,k] for i=P) <= h_k[k])
 
-
-# check 02 for reloading process of truck at cross-docks
 @constraint(cd_modl, reload_check2[k=K],
-                qq_k[k] - sum(r_ik[i,k] for i=P) <= 0)
+                h_k[k] <= sum(u_ik[i,k] for i=P))
 
-# Calculate Finish time of trucks for reloading
-@constraint(cd_modl, reld_s_tm_veh[k=K],
-                l_ik[cross_delivery[1],k]
-                ==  s_k[k]
-                + 1*qq_k[k]
-                + 0.05*sum(d_i[i]*r_ik[i,k] for i=P)
+
+# Calculate start time of departure at cross dock of trucks
+@constraint(cd_modl, leave_cd_time[k=K],
+                s_ik[cd_del_start[1],k]
+                == w_k[k]
+                + 1*h_k[k]
+                + 2*sum(d_i[i]*r_ik[i,k] for i=P)
                 )
-
-
 
 # ****************************************
 # Objective
 #*****************************************
+@objective(cd_modl, Min,
+sum(t_ij[i,j] * x_ijk[i,j,k] for i=N for j=N for k=K))
+
+
+optimize!(cd_modl)
+
+
+if termination_status(cd_modl) == MOI.OPTIMAL
+    optimal_solution = value.(x_ijk)
+    optimal_objective = objective_value(cd_modl)
+elseif termination_status(cd_modl) == MOI.TIME_LIMIT && has_values(model)
+    suboptimal_solution = value.(x_ijk)
+    suboptimal_objective = objective_value(cd_modl)
+else
+    error("The model was not solved correctly.")
+end
